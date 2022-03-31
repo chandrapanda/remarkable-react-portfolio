8a5388adacaeaff09f2f0e4c9f99f6806ff086a4
Chandra Holt <chandra_holt@hotmail.com>
2022-03-31 16:51:07 -0700

    Added initial styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/NavBar";
+import Header from "./components/Header";
+import Card from "./components/Card";
 
+// In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Navbar />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
