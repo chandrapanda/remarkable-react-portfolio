d3c5063acfe831131243fa66b800e0443e49cab0
Chandra Holt <chandra_holt@hotmail.com>
2022-07-11 19:10:09 -0700

    Fixed responsive view for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,7 +24,7 @@ function Projects() {
               rel="noreferrer noopener"
               href={project.link}
               key={project.image}
-              className="w-1/2 p-4"
+              className="sm:w-1/2 w-100 p-4"
             >
               <div className="flex relative">
                 <img
@@ -32,7 +32,7 @@ function Projects() {
                   className="absolute inset-0 w-full h-full object-cover object-center rounded-lg"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <div className="w-1/2 p-4">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
