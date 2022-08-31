dfaf3d8d01df0e2f1f674773eb2902bcf3362b02
Chandra Holt <chandra_holt@hotmail.com>
2022-08-31 14:26:02 -0700

    Updated CV Aug2022

--- src/components/CV.js
+++ src/components/CV.js
@@ -11,7 +11,7 @@ function CV() {
             download="ChandraCV"
             className="w-1/3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
           >
-            Download my CV (updated March 2022)
+            Download my CV (updated August 2022)
           </a>
         </div>
       </div>
