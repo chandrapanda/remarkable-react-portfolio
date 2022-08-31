5419dc8c6e11687961d266d2a1c14314f15470b6
Chandra Holt <chandra_holt@hotmail.com>
2022-08-31 13:43:51 -0700

    Added current CV

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Chandra.
-            <br className="hidden lg:inline-block" />I like to build stuff.
+            <br className="hidden lg:inline-block" /> I like to build stuff.
           </h1>
           <p className="mb-8 leading-relaxed">
             Seattle native turned world explorer turned software engineer.
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -32,7 +32,7 @@ function Projects() {
                   className="absolute inset-0 w-full h-full object-cover object-center rounded-lg"
                   src={project.image}
                 />
-                <div className="w-1/2 p-4">
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
