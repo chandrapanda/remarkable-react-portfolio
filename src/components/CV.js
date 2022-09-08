a0accd3a949823c08e469ee86ac742a1582a77d1
Chandra Holt <chandra_holt@hotmail.com>
2022-09-08 10:54:24 -0700

    Added download icon to div above CV button

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { FolderDownloadIcon } from "@heroicons/react/solid";
 import resume from "../images/full-stack-CV.pdf";
 
 function CV() {
   return (
     <section id="CV" className="text-gray-400 bg-gray-900 body-font">
+      <div className="text-center">
+        <FolderDownloadIcon className="w-10 inline-block mb-0" />
+      </div>
       <div className="container px-5 py-10 mx-auto">
         <div className="text-center mb-20">
           <a
